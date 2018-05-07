ff5e5255d25fd1e63ec0550a3cef303433a05c50
Shane Zhao <shane.zhao@utexas.edu>
2018-05-07 10:49:51 -0500

    changing information revieved between client and server to JSON

--- client/connection-manager.js
+++ client/connection-manager.js
@@ -10,8 +10,30 @@ class ConnectionManager{
         this.connection.addEventListener('open', () => {
             console.log('Connection established');
 
-            this.connection.send('create-session');
+            //this.connection.send('create-session');
+            this.send({
+                type: 'create-session',
+            });
         });
+
+        this.connection.addEventListener('message', event => {
+            console.log('Recevied message', event.data);
+            this.receive(event.data);
+        });
+
+    }
+
+    receive(msg){
+        const data = JSON.parse(msg);
+        if(data.type === 'session-created'){
+            window.location.hash = data.id;
+        }
+    }
+
+    send(data){
+        const msg = JSON.stringify(data);
+        console.log('Sending mssage',msg);
+        this.connection.send(msg);
     }
 
 }
--- server/client.js
+++ server/client.js
@@ -3,6 +3,17 @@ class Client{
         this.connection = connection;
         this.session = null;
     }
+
+    send(data){
+        const msg = JSON.stringify(data);
+        console.log('Sending message', msg);
+        this.connection.send(msg, function ack(err) {
+            if(err){
+                console.error('Message failed', msg, err);
+            }
+        });
+
+    }
 }
 
 module.exports = Client;
